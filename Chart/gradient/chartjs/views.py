8fd0925fcd8f4f7eac32d538a2bb0d605bdd1b61
Iftakhir Mozumder Nibir <134167711+IftakhirNibir@users.noreply.github.com>
2024-02-28 14:50:43 +0600

    update chart project

--- Chart/gradient/chartjs/views.py
+++ Chart/gradient/chartjs/views.py
@@ -3,12 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    a = request.POST.get('text1')
-    b = request.POST.get('text2')
-    c = request.POST.get('text3')
-    d = request.POST.get('text4')
-    e = request.POST.get('text5')
-    f = request.POST.get('text6')
+    if request.method == 'POST':
+        a = request.POST.get('text1','')
+        b = request.POST.get('text2','')
+        c = request.POST.get('text3','')
+        d = request.POST.get('text4','')
+        e = request.POST.get('text5','')
+        f = request.POST.get('text6','')
+    else:
+        a = b = c = d = e = f = ''
     context = {
         'A' : a,
         'B' : b,
